f39323ea1febe4d405f8e7c5caba4cafe18af4ba
SaiKiran2527 <73537513+SaiKiran2527@users.noreply.github.com>
2023-05-02 16:25:32 +0530

    changes(alert) in Home page

--- Ajio/src/app/home/home.component.ts
+++ Ajio/src/app/home/home.component.ts
@@ -7,7 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router) { 
+    alert("Got you my Myannn!!!😆😛")
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
